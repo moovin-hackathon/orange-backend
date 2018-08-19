13e514458abc5582fbe60c468cfd74a18cdd7ad3
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2018-08-19 04:55:32 -0300

    Adiciona entidades

---
+++ app/Subject.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
+}
